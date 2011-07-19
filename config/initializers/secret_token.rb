30b62e4d5713f05e95e9cdba329d55949087ad67
Travis Johnson <travis@roguesharkstudios.com>
2011-07-18 20:23:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gladiator::Application.config.secret_token = '940e4c4b14df36fac6f38f63f9fd287a34175bc85ec2c5099e83859913a4ea750b06bceb94e9cc315686f1efa33e33904bb844d1b0aca10172974d580abb476f'
